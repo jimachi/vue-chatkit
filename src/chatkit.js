3aa26cce9db273ca6a4ff68d586b83503650e38a
Yuichiro Iijima <jimaichiro@gmail.com>
2019-12-27 01:32:05 +0900

    fix a typo

--- src/chatkit.js
+++ src/chatkit.js
@@ -1,16 +1,16 @@
 import { ChatManager, TokenProvider } from "@pusher/chatkit-client";
 
-const INSTANCE_LOCATOR = process.env.VUE_APP_INSTAANCE_LOCATOR;
-const TOKEN_URL = process.env.VUE_APP_TOKEN_URL;
-const MESSAGE_LIMIT = Number(process.env.VUE_APP_MESSAGE_LIMIT) || 10;
+const INSTANCE_LOCATOR = process.env.VUE_APP_INSTANCE_LOCATOR;
+const TOKEN_URL = process.env.VUE_APP_MESSAGE_LIMIT;
+// const MESSAGE_LIMIT = Number(process.env.VUE_APP_MESSAGE_LIMIT) || 10;
 
 let currentUser = null;
-let activeRoom = null;
+// let activeRoom = null;
 
 async function connectUser(userId) {
   const chatManager = new ChatManager({
     instanceLocator: INSTANCE_LOCATOR,
-    TokenProvider: new TokenProvider({ url: TOKEN_URL}),
+    tokenProvider: new TokenProvider({ url: TOKEN_URL}),
     userId
   });
   currentUser = await chatManager.connect();
