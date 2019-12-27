e89f4b738f1d7d7e8e0f2c48773ce1742530e2f3
Yuichiro Iijima <jimaichiro@gmail.com>
2019-12-28 02:07:16 +0900

    make activeRooms feature

--- src/components/RoomList.vue
+++ src/components/RoomList.vue
@@ -8,13 +8,14 @@
                         :active="activeRoom.id === room.id"
                         href="#"
                         @click="onChange(room)">
+      # {{ room.name }}
       </b-list-group-item>
     </b-list-group>
   </div>
 </template>
 
 <script>
-import { mapState } from "vuex"
+import { mapState, mapActions } from "vuex"
 
 export default {
   name: "RoomList",
@@ -23,6 +24,14 @@ export default {
       "rooms",
       "activeRoom"
     ])
+  },
+  methods: {
+    ...mapActions([
+      "changeRoom"
+    ]),
+    onChange(room) {
+      this.changeRoom(room.id)
+    }
   }
 }
 </script>
